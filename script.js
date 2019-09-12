da617721a593f5a54d7944148cbd25647b2f976d
MykolaVitvyskyi <kolya.vit55@gmail.com>
2019-09-12 23:30:19 +0300

    add

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 function checkUrl(url){
-  return /^(http|https):\/\/[^ "]+\.(jpg|jpeg|tiff|tiff|png|gif)$/.test(url);
+  return /^https:\/\/\S+?\.(jpg|jpeg|tiff|tiff|png|gif)$/.test(url);
 }
 
-console.log(checkUrl('www.google.com'));
-console.log(checkUrl('http://www.google.com'));
-console.log(checkUrl('http://www.ololo.jpg'));
+console.log(checkUrl('https://www.ololo.jpg'));
